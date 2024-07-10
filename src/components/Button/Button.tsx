f501572770eb55d3a9b372a1a6f5261a37ca5cfa
Oscar <oscars-email@protonmail.com>
2024-07-10 14:31:44 +0200

    Explanation of style module button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,4 @@
-import styles from './Button.module.css'
+import styles from "./Button.module.css"; //Importing button styles from the module Button.module.css
 
 type Props = {
   data: string;
@@ -6,7 +6,7 @@ type Props = {
 };
 
 function Button({ data, func }: Props) {
-  const class_name = [styles.button, styles.padded].join(' ')
+  const class_name = [styles.button, styles.padded].join(" "); //Creating a variable 'class_name' so that our button gets those classes => className = {class_name}
   return (
     <button type="button" onClick={func} className={class_name}>
       {data}
